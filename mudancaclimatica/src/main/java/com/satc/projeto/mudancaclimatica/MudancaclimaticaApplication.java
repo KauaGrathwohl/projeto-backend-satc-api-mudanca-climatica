5d5b2058a4cd7a428a8f68cdac6c3598873ec05c
Iuri de Lima Marques <iuri.marques@betha.com.br>
2024-06-20 22:17:22 -0300

    master: configurado ambiente com o docker

---
+++ mudancaclimatica/src/main/java/com/satc/projeto/mudancaclimatica/MudancaclimaticaApplication.java
@@ -0,0 +1,13 @@
+package com.satc.projeto.mudancaclimatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudancaclimaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudancaclimaticaApplication.class, args);
+	}
+
+}
---
+++ mudancaclimatica/src/test/java/com/satc/projeto/mudancaclimatica/MudancaclimaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satc.projeto.mudancaclimatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudancaclimaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
