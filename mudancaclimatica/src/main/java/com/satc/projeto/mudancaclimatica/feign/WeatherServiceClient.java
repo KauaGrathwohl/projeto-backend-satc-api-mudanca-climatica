bb7ea1f8d5de95019cd9f2b3739b32da4047babf
Iuri de Lima Marques <iuri.marques@betha.com.br>
2024-06-26 19:27:25 -0300

    main: remocao de classe desnecessaria

--- mudancaclimatica/src/main/java/com/satc/projeto/mudancaclimatica/feign/WeatherServiceClient.java
+++
@@ -1,7 +0,0 @@
-package com.satc.projeto.mudancaclimatica.feign;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "WeatherServiceClient", url = "https://api.open-meteo.com/v1/forecast")
-public interface WeatherServiceClient {
-}
